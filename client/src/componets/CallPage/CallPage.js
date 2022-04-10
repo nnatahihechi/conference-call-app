54cf6362aa3eb6b0f4795dd63c56655e1eadeeef
IHECHI <nnatahi57@gmail.com>
2022-04-10 12:17:37 +0100

    update

--- client/src/componets/CallPage/CallPage.js
+++ client/src/componets/CallPage/CallPage.js
@@ -1,8 +1,51 @@
+import{ FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import './CallPage.scss'
-const CallPage = () =>{
+
+
+const CallPageFooter = () => {
     return (
-        <h1>Callpage</h1>
+        <div className="footer-item">
+        <div className="left-item">
+        <div className="icon-block">
+meeting details
+<FontAwesomeIcon className="icon" icon={faAngleUp}/>
+        </div>
+        </div>
+    
+        
+        <div className="center-item">
+            <div className="icon-block">
+                <FontAwesomeIcon className="icon" icon={faMicrophone}/>
+            </div>
+            </div>
+            
+            <div className="icon-block">
+                <FontAwesomeIcon className="icon" icon={faPhone}/>
+            </div>
+            
+            <div className="icon-block">
+                <FontAwesomeIcon className="icon" icon={faVideo}/>
+            </div>
+
+           
+           <div className="right-item">
+               <div className="icon-block">
+                   <FontAwesomeIcon className="icon red" icon={faClosedCaptioning}/>
+                   <p className="title">Turn on Caption </p>
+                   </div>
+                   
+            <div className="icon-block">
+                   <FontAwesomeIcon className="icon red" icon={faClosedCaptioning}/>
+                   <p className="title"> Present now </p>
+                    </div>
+               </div>
+               
+            </div>
+        
     )
+
 }
 
-export default CallPage
+
+export default CallPageFooter
+
--- client/src/componets/NoMatch/NoMatch.js
+++ client/src/componets/NoMatch/NoMatch.js
@@ -1,7 +1,19 @@
 import './NoMatch.scss'
+// import Header './.....................'
 const NoMatch = () =>{
     return (
-        <h1>NoMatch</h1>
+  <div className="no-match">
+    <header/>
+    <div className="no-match__contain">
+    <h2>invaild video call name</h2>
+    <div className="action-btn">
+        <Link className="btn green" to='./'>
+        Return to home screen 
+        </Link>
+    </div>
+    </div>
+    </div>
+
     )
 }
 
